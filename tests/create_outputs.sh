28ac6e367f9f837628961c3bbff57e255d354bc4
Peter Kral <peter.kral@pdfix.net>
2025-08-05 14:56:23 +0200

    PVQ-4019 External actions - functional tests - functional testing was moved to internal framework and is using internal files - removing functional testing from action

--- tests/create_outputs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Go to test framework directory
-cd ../../test-framework
-
-# Create outputs for tests
-./create_outputs.sh ../action-autotag-textract-docker/tests/configs/test_autotag.json
-./create_outputs.sh ../action-autotag-textract-docker/tests/configs/test_template.json
--- tests/run_functional_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Go to test framework directory
-cd ../../test-framework
-
-# Run tests with path to folder with configs
-./run_tests.sh ../action-autotag-textract-docker/tests/configs
